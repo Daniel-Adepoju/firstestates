5e38c0f87d8c29957a7864964666193844dc8c8f
Daniel-Adepoju <dannymay116@gmail.com>
2025-07-17 00:05:53 +0100

    add appointment, new cron jobs, info modal

--- app/(root)/test/page.tsx
+++ app/(root)/test/page.tsx
@@ -21,27 +21,7 @@ if(!mounted) return null
 
   return (
     <>
-
-      <div className="flex flex-col items-center  mt-45 mx-auto w-full">
-   
-    <Calendar 
-    mode="single"
-    defaultMonth={date}
-    selected={date}
-    onSelect={setDate}
-    captionLayout="dropdown"
-    className="dark:bg-darkGray bg-white shadow-md "
-    classNames={{
-    day: "rounded-md p-1 md:p-2 mx-[2px] my-[2px] hover:bg-muted", // add spacing
-  }}
-    />
-
-    <div>
-      {createdAt(date).includes('ago') ?
-      <span>You had an appointment {createdAt(date)}</span>
-      :  <span>You have an appointment {createdAt(date)}</span> }
-      </div>
-         </div>
+<div>Test</div>
           </>
   )
 }
