828350e6cad00a46f42e623995b6ed56168aa221
Daniel-Adepoju <dannymay116@gmail.com>
2026-06-04 16:22:42 +0100

    add fix to email template subject size  ui

--- app/lib/server/sendEmail.js
+++ app/lib/server/sendEmail.js
@@ -33,7 +33,7 @@ export const sendEmail = async ({ to, subject, message }) => {
             
             <!-- Header -->
             <tr>
-              <td style="background-color:rgb(8,116,199); padding:22px; text-align:center; color:#fff;">
+              <td style="background-color:rgb(8,116,199); padding:15px; text-align:center; color:#fff;">
                 <h1 style="margin:0; font-size:20px;
                 font-weight:700; 
                 letter-spacing:0.3px;">
